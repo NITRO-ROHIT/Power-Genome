77ff9184356a01d1ad344e5767fbcbffd911c4cb
NITRO-ROHIT <103458323+NITRO-ROHIT@users.noreply.github.com>
2022-11-05 16:54:19 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="powergenome",
+    packages=find_packages(),
+    version="0.5.6",
+    description="Extract PUDL data for use in power system models",
+    author="Greg Schivley",
+    entry_points={
+        "console_scripts": [
+            "run_powergenome_multiple = powergenome.run_powergenome_multiple_outputs_cli:main",
+        ]
+    },
+)
